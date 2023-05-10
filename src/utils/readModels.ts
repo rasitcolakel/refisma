585e2125dc0fec7e4e205b3ddd139e289e128fab
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-05-10 18:57:38 +0300

    readModels prettied

--- src/utils/readModels.ts
+++ src/utils/readModels.ts
@@ -1,97 +1,97 @@
 // read models from  prisma/schema.prisma
 
-import { readFileSync } from "fs";
+import { readFileSync } from 'fs'
 
 export const getModels = (): Model[] => {
-  const schema = readFileSync("prisma/schema.prisma", "utf-8");
-  const models = parseSchema(schema);
+    const schema = readFileSync('prisma/schema.prisma', 'utf-8')
+    const models = parseSchema(schema)
 
-  return models;
-};
+    return models
+}
 
 export const parseSchema = (schema: string): Model[] => {
-  const models: Model[] = [];
+    const models: Model[] = []
 
-  const lines = schema.split("\n");
-  let model: Model | undefined = undefined;
+    const lines = schema.split('\n')
+    let model: Model | undefined = undefined
 
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    if (line.startsWith("//") || line.trim().startsWith("@")) continue;
-    if (line.startsWith("model ")) {
-      const name = line.split(" ")[1];
-      model = {
-        name,
-        fields: [],
-      };
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i]
+        if (line.startsWith('//') || line.trim().startsWith('@')) continue
+        if (line.startsWith('model ')) {
+            const name = line.split(' ')[1]
+            model = {
+                name,
+                fields: [],
+            }
+        }
+
+        if (line.startsWith('}')) {
+            if (model) {
+                models.push(model)
+                model = undefined
+            }
+        }
+
+        if (line.startsWith('  ')) {
+            if (model) {
+                const field = parseField(line)
+                model.fields.push(field)
+            }
+        }
     }
 
-    if (line.startsWith("}")) {
-      if (model) {
-        models.push(model);
-        model = undefined;
-      }
-    }
+    return models
+}
 
-    if (line.startsWith("  ")) {
-      if (model) {
-        const field = parseField(line);
-        model.fields.push(field);
-      }
-    }
-  }
+export type Model = {
+    name: string
+    fields: Field[]
+}
 
-  return models;
-};
-
-type Model = {
-  name: string;
-  fields: Field[];
-};
-
-type Field = {
-  name: string;
-  type: string;
-  isList: boolean;
-  isRequired: boolean;
-  isUnique: boolean;
-  isId: boolean;
-  isUpdatedAt: boolean;
-  isCreatedAt: boolean;
-  isOptional: boolean;
-  isReadOnly: boolean;
-  isGenerated: boolean;
-};
+export type Field = {
+    name: string
+    type: string
+    isList: boolean
+    isRequired: boolean
+    isUnique: boolean
+    isId: boolean
+    isUpdatedAt: boolean
+    isCreatedAt: boolean
+    isOptional: boolean
+    isReadOnly: boolean
+    isGenerated: boolean
+}
 
 const parseField = (line: string): Field => {
-  // replace all spaces with a single space
-  const arrayLine = line.replace(/\s+/g, " ").trim();
+    // replace all spaces with a single space
+    const arrayLine = line.replace(/\s+/g, ' ').trim()
 
-  const [name, type, ...options] = arrayLine.split(" ") as string[];
-  console.log({ name, type, options });
-  console.log("type of type", typeof type.startsWith);
+    const [name, type, ...options] = arrayLine.split(' ') as string[]
+    console.log({ name, type, options })
+    console.log('type of type', typeof type.startsWith)
 
-  const isList = type.startsWith("[") && type.endsWith("]");
-  const isRequired = type.endsWith("!");
-  const isUnique = options.includes("@unique");
-  const isId = options.includes("@id");
-  const isUpdatedAt = options.includes("@updatedAt");
-  const isCreatedAt = options.includes("@createdAt");
-  const isOptional = options.includes("@optional");
-  const isReadOnly = options.includes("@readOnly");
-  const isGenerated = options.includes("@generated");
+    const isList = type.startsWith('[') && type.endsWith(']')
+    const isRequired = type.endsWith('!')
+    const isUnique = options.includes('@unique')
+    const isId = options.includes('@id')
+    const isUpdatedAt = options.includes('@updatedAt')
+    const isCreatedAt = options.includes('@createdAt')
+    const isOptional = options.includes('@optional')
+    const isReadOnly = options.includes('@readOnly')
+    const isGenerated = options.includes('@generated')
 
-  return {
-    name,
-    type,
-    isList,
-    isRequired,
-    isUnique,
-    isId,
-    isUpdatedAt,
-    isCreatedAt,
-    isOptional,
-    isReadOnly,
-    isGenerated,
-  };
-};
+    return {
+        name,
+        type,
+        isList,
+        isRequired,
+        isUnique,
+        isId,
+        isUpdatedAt,
+        isCreatedAt,
+        isOptional,
+        isReadOnly,
+        isGenerated,
+    }
+}
