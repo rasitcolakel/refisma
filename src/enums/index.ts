3e780b16e38af87d66bf7592d4474ecfce3e857f
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-05-12 23:29:54 +0300

    Enums added

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,7 +1,45 @@
 export enum MethodNames {
-    CREATE = 'create',
-    GET = 'get',
-    GET_BY_ID = 'getById',
-    UPDATE = 'update',
-    DELETE = 'delete',
+    create = 'create',
+    findMany = 'findMany',
+    getById = 'getById',
+    update = 'update',
+    delete = 'delete',
+}
+export type TMethodNames = keyof typeof MethodNames
+
+export enum PrismaScalarTypes {
+    String = 'String',
+    Boolean = 'Boolean',
+    Int = 'Int',
+    BigInt = 'BigInt',
+    Float = 'Float',
+    Decimal = 'Decimal',
+    DateTime = 'DateTime',
+    Json = 'Json',
+    Bytes = 'Bytes',
+    Unsupported = 'Unsupported',
+}
+
+export type TPrismaScalarTypes = keyof typeof PrismaScalarTypes
+
+export enum PrismaRelationTypes {
+    OneToOne = 'OneToOne',
+    OneToMany = 'OneToMany',
+    ManyToMany = 'ManyToMany',
+}
+
+export enum PrismaAttributes {
+    Id = '@id',
+    IId = '@@id',
+    Default = '@default',
+    Unique = '@unique',
+    UUnique = '@@unique',
+    IIndex = '@@index',
+    Relation = '@relation',
+    Map = '@map',
+    MMap = '@@map',
+    UpdatedAt = '@updatedAt',
+    Ignore = '@ignore',
+    IIgnore = '@@ignore',
+    Schema = '@schema',
 }
