136615eefe1c95f73dec11fcce55668e096fff5d
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-05-21 15:57:35 +0300

    Edit Page added and PUT converted to PATCH

--- src/utils/generateService.ts
+++ src/utils/generateService.ts
@@ -7,7 +7,7 @@ import prettier from 'prettier'
 import { MethodNames } from '../enums'
 import { generateApiFile } from './generateApiFiles'
 import { generateZodFile } from './generateZodSChemas'
-import { generateRefineCreatePage, generateRefineListPage } from './generateRefinePages'
+import { generateRefineCreatePage, generateRefineEditPage, generateRefineListPage } from './generateRefinePages'
 export const generateService = (model: Model) => {
     const template = readFileSync(path.join(__dirname, '../../templates', 'service.ts.hbs'), 'utf-8')
     const templateCompiler = handlebars.compile(template)
@@ -80,7 +80,7 @@ export const generateService = (model: Model) => {
             },
             {
                 isSingular: true,
-                method: 'PUT',
+                method: 'PATCH',
                 name: MethodNames.update,
                 params: [
                     {
@@ -163,6 +163,7 @@ export const generateService = (model: Model) => {
 
     generateRefineListPage(model, templateParams)
     generateRefineCreatePage(model, templateParams)
+    generateRefineEditPage(model, templateParams)
 
     return templateCompiler
 }
