e1f01870f525a9edb5b677b53bd391679cd98567
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-05-28 12:08:21 +0300

    Example page to examples

---
+++ examples/ant-design-next-js/src/contexts/index.tsx
@@ -0,0 +1,64 @@
+import { RefineThemes } from "@refinedev/antd";
+import { ConfigProvider, theme } from "antd";
+import { parseCookies } from "nookies";
+import React, {
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from "react";
+
+type ColorModeContextType = {
+  mode: string;
+  setMode: (mode: string) => void;
+};
+
+export const ColorModeContext = createContext<ColorModeContextType>(
+  {} as ColorModeContextType
+);
+
+export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
+  children,
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [mode, setMode] = useState("light");
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      setMode(parseCookies()["theme"]);
+    }
+  }, [isMounted]);
+
+  const setColorMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+    } else {
+      setMode("light");
+    }
+  };
+
+  const { darkAlgorithm, defaultAlgorithm } = theme;
+
+  return (
+    <ColorModeContext.Provider
+      value={{
+        setMode: setColorMode,
+        mode,
+      }}
+    >
+      <ConfigProvider
+        // you can change the theme colors here. example: ...RefineThemes.Magenta,
+        theme={{
+          ...RefineThemes.Blue,
+          algorithm: mode === "light" ? defaultAlgorithm : darkAlgorithm,
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </ColorModeContext.Provider>
+  );
+};
